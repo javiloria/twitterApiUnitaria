8307d2943520183952a946b65eb5dab6921b351b
javiloria <34045912+javiloria@users.noreply.github.com>
2017-12-10 17:34:06 -0400

    Add files via upload

--- prueba.php
+++ prueba.php
@@ -10,14 +10,19 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 public function setUp(){
 define('CONSUMER_KEY', 'gAgy6UZjObmhYpncLxkywQFHa');
 define('CONSUMER_SECRET', 'qp4Xmo6vXsB0at4JuprViCYijZJ8QcZpLBHvsLkiPlF1QYCTDB');
-define('OAUTH_CALLBACK','salida.php');
-$access_token='79051477-tAUT7OxYUuB7p5NArzqu2Iv0mlLNSTsJPly2A8nD4';
-$access_token_secret='zRTQaaiVBUg0CXMn9XbVCkIT7Jk76SolntDr0Wg6GBZza';
-$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token, $access_token_secret);
-$connection->setTimeouts(10, 15);
-$content = $connection->get("account/verify_credentials");
+define('OAUTH_CALLBACK','https://twittering.herokuapp.com/callback.php');
+echo "string <br>";
 
-$url = $connection->url("oauth/authorize", ["oauth_token" => "zRTQaaiVBUg0CXMn9XbVCkIT7Jk76SolntDr0Wg6GBZza"]);
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+echo "aaa <br>";
+$request_token = $connection->oauth('oauth/request_token', array ('oauth_callback' => OAUTH_CALLBACK));
+echo "ee<br>";
+$_SESSION['oauth_token'] = $request_token['oauth_token'];
+echo "aaaasdd<br>";
+$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+echo "ssssssssssssss<br>";
+
+$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
 
 echo $url;
 }
